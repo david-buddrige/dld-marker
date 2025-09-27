e83cabc68f401da99db0a47bfc97a69d76864b29
david-buddrige <169033574+david-buddrige@users.noreply.github.com>
2025-09-27 14:56:59 +0800

    updated tests

--- heading_test.py
+++ heading_test.py
@@ -42,3 +42,29 @@ try:
     print("<h3> passed")
 except:
     print("<h3> did not pass")
+    
+    
+    
+found_heading = heading_finder.find_heading(heading, heading)
+try:
+    assert found_heading == False
+    print("<h3> passed")
+except:
+    print("<h3> did not pass")
+    
+html = "<p>" + heading + "</p>"
+found_heading = heading_finder.find_heading(heading, html)
+try:
+    assert found_heading == False
+    print("<p> passed")
+except:
+    print("<p> did not pass")
+
+html = "<h4>" + heading + "</h4>"
+found_heading = heading_finder.find_heading(heading, html)
+try:
+    assert found_heading == True
+    print("<h4> passed")
+except:
+    print("<h4> did not pass")
+
